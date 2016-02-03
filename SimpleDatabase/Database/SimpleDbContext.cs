c76135fc565bb979c15ae492030ed54cdbd20288
bulbulat <krzysk46@gmail.com>
2016-02-03 11:19:32 +0100

    add files

---
+++ SimpleDatabase/Database/SimpleDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDatabase.Database
+{
+    class SimpleDbContext:DbContext
+    {
+        public SimpleDbContext():base()
+        {
+
+        }
+        public DbSet<Models.User> Users { get; set; }
+        
+    }
+}
---
+++ SimpleDatabase/Interface1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimpleDatabase
+{
+    interface Interface1
+    {
+        void Show();
+        
+    }
+
+    abstract public class Teacher
+    {
+        public void DoNothing()
+        {
+            Console.WriteLine();
+        }
+    }
+}
